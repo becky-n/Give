81b63134c5c3edcd1e74d59b6e0bb2900c16e94c
Becky <beckyngan28@gmail.com>
2025-08-12 13:26:11 +1200

    fixing tailwing configuiration for setup

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        backgroundGrey: "#F5F3F3",
+        defaultPink: "#F5CCE2",
+        lightPink: "rgba(245, 204, 226, 0.4)", // 40% opacity
+        defaultYellow: "#FFDD4A",
+        lightYellow: "rgba(255, 221, 74, 0.15)", // 15% opacity
+        darkGrey: "#D9D9D9",
+      },
+    },
   },
   plugins: [],
-}
-
+};
